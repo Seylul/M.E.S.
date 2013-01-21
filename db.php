11146a31b8c74ac166dfb2ff3e8738f226e438b5
Semih Özköroğlu <semih.ozkoroglu@bil.omu.edu.tr>
2013-01-21 18:40:45 +0200

    pagoda db information

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-	$connect = mysql_connect('localhost','root','semihsemih');
+	$connect = mysql_connect('tunnel.pagodabox.com:3306','robbin','1HmfM2oj');
 	$db	 = mysql_select_db('eylul');
 ?>
